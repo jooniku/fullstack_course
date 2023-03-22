0308c2db4d0fbbe717a0f250141f6ff600b30b8c
Joona Nikula <nikulajoona@gmail.com>
2023-03-22 13:18:43 +0200

    k

---
+++ part1/course_information/src/App.js
@@ -0,0 +1,48 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.first + props.second + props.third}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header course={course} />
+      <Content name={part1} exercises={exercises1} />
+      <Content name={part2} exercises={exercises2} />
+      <Content name={part3} exercises={exercises3} />
+      <Total first={exercises1} second={exercises2} third={exercises3} />
+    </>
+  )
+}
+
+export default App;
